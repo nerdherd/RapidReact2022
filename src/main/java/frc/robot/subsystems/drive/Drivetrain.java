80a6f4436b21f49eb9ed5218ca971aec69ec95d3
samanthac02 <samanthaylchang+github@gmail.com>
2022-02-28 21:53:01 -0800

    Cleaned up pnuematics files

--- src/main/java/frc/robot/subsystems/drive/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/Drivetrain.java
@@ -5,8 +5,10 @@ import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.OI;
@@ -18,68 +20,65 @@ public class Drivetrain {
     public static TalonFX leftSlave; // Channel 17 on CAN, 1 on PDP
 
     public static Compressor compressor; // Channel 3 on CAN
-    private static Piston leftShifter; // Channels 2 and 5
-    private static Piston rightShifter; // Channels 1 and 4
+    private static DoubleSolenoid leftShifter; // Channels 2 and 5
+    private static DoubleSolenoid rightShifter; // Channels 1 and 4
     
     // ======================= TELEOP FUNCTIONS ======================= //
     
     public static void setupDrivetrain() {
-        rightMaster = new TalonFX(30);
-        leftMaster = new TalonFX(16);
-        rightSlave = new TalonFX(31);  
-        leftSlave = new TalonFX(17);
+      rightMaster = new TalonFX(30);
+      leftMaster = new TalonFX(16);
+      rightSlave = new TalonFX(31);  
+      leftSlave = new TalonFX(17);
 
-        leftSlave.follow(leftMaster);
-        rightSlave.follow(rightMaster);
+      leftSlave.follow(leftMaster);
+      rightSlave.follow(rightMaster);
 
-        // Inverted the right side
-        rightMaster.setInverted(true);
-        leftSlave.setInverted(InvertType.FollowMaster);
-        rightSlave.setInverted(InvertType.FollowMaster);
+      // Inverted the right side
+      rightMaster.setInverted(true);
+      leftSlave.setInverted(InvertType.FollowMaster);
+      rightSlave.setInverted(InvertType.FollowMaster);
 
-        // Pneumatics setup
-        compressor = new Compressor(3, PneumaticsModuleType.CTREPCM);
-        leftShifter = new Piston(3, PneumaticsModuleType.CTREPCM, 2, 5);
-        rightShifter = new Piston(3, PneumaticsModuleType.CTREPCM, 1, 4);
+      // Pneumatics setup
+      compressor = new Compressor(3, PneumaticsModuleType.CTREPCM);
+      leftShifter = new DoubleSolenoid(3, PneumaticsModuleType.CTREPCM, 2, 5);
+      rightShifter = new DoubleSolenoid(3, PneumaticsModuleType.CTREPCM, 1, 4);
     }
 
     public static void driveControllerMovement() {
-        double leftInput = OI.ps4Controller.getLeftY();
-        double rightInput = OI.ps4Controller.getRightY();
-    
-        rightMaster.set(ControlMode.PercentOutput, rightInput);
-        leftMaster.set(ControlMode.PercentOutput, leftInput);
-    
-        // Gear shifting
-        // Actually triangle button
-        if (OI.ps4Controller.getTriangleButtonPressed()) {
-          // Shifts to high gear
-          leftShifter.setForwards();
-          rightShifter.setForwards();
-          SmartDashboard.putString(" Button State ", "A");
-          SmartDashboard.putBoolean(" Shifter ", leftShifter.isForwards());
-        }
+      double leftInput = OI.ps4Controller.getLeftY();
+      double rightInput = OI.ps4Controller.getRightY();
+  
+      rightMaster.set(ControlMode.PercentOutput, rightInput);
+      leftMaster.set(ControlMode.PercentOutput, leftInput);
+  
+      // Gear shifting
+      // Actually triangle button
+      if (OI.ps4Controller.getTriangleButtonPressed()) {
+        // Shifts to high gear
+        leftShifter.set(Value.kForward);
+        rightShifter.set(Value.kForward);
+        SmartDashboard.putString(" Button State ", "A");
+      }
 
-        // Actually square button
-        if (OI.ps4Controller.getCircleButtonPressed()) {
-          // Shifts to low gear
-          leftShifter.setReverse();
-          rightShifter.setReverse();
-          SmartDashboard.putString(" Button State ", "B");
-          SmartDashboard.putBoolean(" Shifter ", leftShifter.isReverse());
-        }
-
+      // Actually square button
+      if (OI.ps4Controller.getCircleButtonPressed()) {
+        // Shifts to low gear
+        leftShifter.set(Value.kReverse);
+        rightShifter.set(Value.kReverse);
+        SmartDashboard.putString(" Button State ", "B");
+      }
     }
 
     public static void updateSmartDashboardForDrivetrain() {
-        SmartDashboard.putNumber(" Right Master Current ", rightMaster.getSupplyCurrent());
-        SmartDashboard.putNumber(" Right Slave Current ", rightSlave.getSupplyCurrent());
-        SmartDashboard.putNumber(" Left Master Current ", leftMaster.getSupplyCurrent());
-        SmartDashboard.putNumber(" Left Slave Current ", leftSlave.getSupplyCurrent());
-
-        SmartDashboard.putBoolean(" Compressor Enabled ", compressor.enabled());
-        SmartDashboard.putBoolean(" Pressure Switch ", compressor.getPressureSwitchValue());
-        SmartDashboard.putNumber(" Compressor Current ", compressor.getCurrent());
+      SmartDashboard.putNumber(" Right Master Current ", rightMaster.getSupplyCurrent());
+      SmartDashboard.putNumber(" Right Slave Current ", rightSlave.getSupplyCurrent());
+      SmartDashboard.putNumber(" Left Master Current ", leftMaster.getSupplyCurrent());
+      SmartDashboard.putNumber(" Left Slave Current ", leftSlave.getSupplyCurrent());
+        
+      SmartDashboard.putBoolean(" Compressor Enabled ", compressor.enabled());
+      SmartDashboard.putBoolean(" Pressure Switch ", compressor.getPressureSwitchValue());
+      SmartDashboard.putNumber(" Compressor Current ", compressor.getCurrent());
     }
 
     // ====================== AUTONOMOUS FUNCTIONS ====================== //
