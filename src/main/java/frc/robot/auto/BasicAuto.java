9d9c76238c9fc27785eaf85d1058b99f5e30d5aa
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-03-18 17:10:14 -0700

    Move auto selector out of periodic

---
+++ src/main/java/frc/robot/auto/BasicAuto.java
@@ -0,0 +1,22 @@
+// package frc.robot.auto;
+
+// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+// import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+// import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+// import frc.robot.constants.EverybotConstants;
+// import frc.robot.everybot.EverybotIntake;
+// import frc.robot.Robot;
+// import frc.robot.commands.OpenLoopDrive;
+// import frc.robot.commands.Outtake;
+
+// public class BasicAuto extends SequentialCommandGroup {
+//     public BasicAuto() {
+//         addCommands(
+//             new Outtake(EverybotConstants.kEverybotAutoOuttake, Robot.everybotIntake)//,
+//             // new ParallelRaceGroup(new OpenLoopDrive(0.5), new WaitCommand(1))
+//         );
+//     }
+// }
