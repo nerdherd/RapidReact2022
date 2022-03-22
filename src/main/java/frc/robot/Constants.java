95111250d22d270987aee1ad1949ed01004b55f6
easternspork <fumi.okutsu@gmail.com>
2022-03-22 09:54:32 -0700

    Move elevator constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,30 +50,35 @@ public final class Constants {
     }
     
     public static final class ClimberConstants {
-        public static final double kArmkP = 0.04;
+        public static final double kArmkP = 2.0; //0.5
         public static final double kArmkI = 0;
-        public static final double kArmkD = 0;
-        public static final double kArmkF = 0; 
+        public static final double kArmkD = 0.6; //0.6 
+        public static final double kArmkF = 2.82; // 2.82
 
-        public static final double kArmStaticGain = 0.0;
-        public static final double kArmGravityGain = 0.11;
-        public static final double kArmVelocityGain = 0;
-
-        public static final double kArmGravityFF = 0.11;
+        public static final double kArmGravityFF = 0.5; //0.11 voltage calc, 0.41 phys calc
         public static final double kArmStaticFF = 0.0;
 
-        public static final double kArmCruiseVelocity = 40;
-        public static final double kArmMotionAcceleration = 20;
-        public static final double kArmMaxVelocity = 54.0;
-        public static final double kArmMaxAcceleration = 54.0;
-        public static final double kArmDeadband = 0.0004;
+        public static final double kArmCruiseVelocity = 250;
+        public static final double kArmMotionAcceleration = 125;
+        public static final double kArmMaxVelocity = 575;
+        public static final double kArmMaxAcceleration = 575;
+        public static final double kArmDeadband = 0.004;
         public static final double kArmStaticFrictionDeadband = 5.0;
 
-        public static final double kArmAngleOffset = 60;
+        public static final double kArmAngleOffset = 42;
         public static final double kArmAngleLength = 1.0 / 4096 * 360 * 29;
 
-        public static final double kTicksToRungAngle = -200; //Actual ticks 620, not resetting encoder position properly
-        public static final double kTicksToVertical = -60;
+        public static final double kTicksToRungAngle = 200; //Actual ticks 620, not resetting encoder position properly
+        public static final double kTicksToClearRung = 250;
+        public static final double kTicksToVertical = 60;
+        public static final double kArmTicksDownSoftLimit = 330;
+        public static final double kArmTicksUpSoftLimit = 0;
+        public static final double kElevatorTicksUp = 20600;
+        public static final double kElevatorTicksDown = 2000;
+
+        public static final double kSoftLimitTicks = 20000;
+
+        public static final double kOperatorAlpha = 0.11765;
+        public static final double kOperatorOneMinusAlpha = 0.88235;
     }
-    
 }
