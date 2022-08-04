54d1f8bd877840021990636cfcb554fc6d122efb
Tiffany C <tchen1914@gmail.com>
2022-08-04 09:47:51 -0700

    Added everybot climber system check

---
+++ src/main/java/frc/robot/subsystems/climber/commands/systemchecks/EverybotClimberTest.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.climber.commands.systemchecks;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.EverybotConstants;
+import frc.robot.subsystems.EverybotClimber;
+
+public class EverybotClimberTest extends SequentialCommandGroup {
+
+    public static RobotContainer robotContainer = new RobotContainer();
+    public static EverybotClimber everybotClimber = new EverybotClimber();
+
+    public EverybotClimberTest() {
+
+        addCommands(
+            new InstantCommand(() -> everybotClimber.moveClimber(1 * EverybotConstants.kTicksToLowRung)),
+            new InstantCommand(() -> everybotClimber.moveClimber(1 * EverybotConstants.kTicksToClimbLowRung)),
+            new InstantCommand(() -> everybotClimber.moveClimber(1 * EverybotConstants.kTicksToHome))
+        );
+
+    }
+
+    
+}
